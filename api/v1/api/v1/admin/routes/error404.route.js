97189497613fade206378e598315b59fb69ea0cb
phamhuynh <huynhpham493@gmail.com>
2025-11-15 23:14:25 +0700

    update

--- api/v1/api/v1/admin/routes/error404.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const controller = require("../controllers/error404.controller")
-
-router.get("/", controller.index)
-
-module.exports = router;
--- api/v1/api/v1/admin/routes/notification.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const controller = require("../controllers/notification.controller")
-
-router.get("/", controller.index)
-router.post("/postQuickOrder", controller.PostQuickOrder)
-
-module.exports = router;
